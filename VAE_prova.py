42dc2b6e8b6f6d545cd31afcd3322b87a6c7464f
damns99 <lucarelli.damiano.ld@gmail.com>
2022-01-27 17:46:14 +0100

    log_variance + readability

--- VAE_prova.py
+++ VAE_prova.py
@@ -1,7 +1,7 @@
+import os.path
+
 import h5py
 import numpy as np
-import matplotlib.pyplot as plt
-import os.path
 
 from tensorflow.keras.layers import Input, Dense, Dropout, Flatten, Lambda, Reshape
 from tensorflow.keras.models import Model
@@ -12,60 +12,71 @@ jetList = np.array([])
 datafiles = ['~/Documents/CMEPDA/CMEPDA_exam/Data/jetImage_7_100p_30000_40000.h5']
 for fileIN in datafiles:
     f = h5py.File(os.path.expanduser(fileIN))
-    # for pT, etarel, phirel
-    myJetList = np.array(f.get("jetConstituentList")[:,:,[5,8,11]])
+    # for pT, etarel, phirel [5, 8, 11]
+    myJetList = np.array(f.get("jetConstituentList")[:, :, [5, 8, 11]])
     jetList = np.concatenate([jetList, myJetList], axis=0) if jetList.size else myJetList
     del myJetList
     f.close()
 
+pt_norm = 100.
+jetList[:, :, 0] = jetList[:, :, 0] / pt_norm
+
+njets = jetList.shape[0]
+jet_shape = jetList.shape[1:]
+
 def sample_latent_features(distribution):
-    distribution_mean, distribution_variance = distribution
-    distribution_variance = tf.square(distribution_variance)
-    batch_size = tf.shape(distribution_variance)[0]
-    random = tf.keras.backend.random_normal(shape=(batch_size, tf.shape(distribution_variance)[1]))
-    return distribution_mean + tf.exp(-0.5 * distribution_variance) * random
+    mean, log_variance = distribution
+    random = tf.random.normal(shape=tf.shape(log_variance))
+    return mean + tf.exp(1/2 * log_variance) * random
 
 def kl_divergence_normal(distribution):
-    distribution_mean, distribution_variance = distribution
-    distribution_variance = tf.square(distribution_variance)
-    return 1/2 * (distribution_variance + tf.square(distribution_mean) -1 - tf.math.log(distribution_variance))
+    mean, log_variance = distribution
+    return 1/2 * (tf.exp(log_variance) + tf.square(mean) -1 - log_variance)
 
 enc_dimensions = 2
 
-encoder_input = Input(shape = jetList.shape[1:])
+encoder_input = Input(shape=jet_shape)
 hidden = Flatten()(encoder_input)
-hidden = Dense(64, activation="relu")(hidden)
 hidden = Dense(32, activation="relu")(hidden)
 hidden = Dense(16, activation="relu")(hidden)
 hidden = Dense(8, activation="relu")(hidden)
 
-distribution_mean = Dense(2, name = 'mean')(hidden)
-distribution_variance = Dense(2, name = 'log_variance')(hidden)
-latent_encoding = Lambda(sample_latent_features, name = 'latent_encoding')([distribution_mean, distribution_variance])
-kl_divergence = Lambda(kl_divergence_normal, name = 'kl_divergence')([distribution_mean, distribution_variance])
+mean_layer = Dense(enc_dimensions, activation="relu", name='mean')(hidden)
+log_variance_layer = Dense(enc_dimensions, activation="relu", name='log_variance')(hidden)
+
+latent_encoding = Lambda(sample_latent_features,
+                         name='latent_encoding')([mean_layer, log_variance_layer])
+kl_divergence = Lambda(kl_divergence_normal,
+                       name='kl_divergence')([mean_layer, log_variance_layer])
 
 decoder_input = latent_encoding
 hidden = Dense(8, activation="relu")(decoder_input)
 hidden = Dense(16, activation="relu")(hidden)
 hidden = Dense(32, activation="relu")(hidden)
-hidden = Dense(64, activation="relu")(hidden)
-hidden = Dense(100*3, activation="relu")(hidden)
-decoder_output = Reshape(target_shape = (100, 3), name = 'decoder_output')(hidden)
+hidden = Dense(np.prod(jet_shape), activation="relu")(hidden)
+decoder_output = Reshape(target_shape=jet_shape, name='decoder_output')(hidden)
 
-autoencoder = Model(inputs = encoder_input, outputs = [decoder_output, kl_divergence], name = 'autoencoder')
+autoencoder = Model(inputs=encoder_input, outputs=[decoder_output, kl_divergence],
+                    name='autoencoder')
 autoencoder.summary()
 
-encoder_model = Model(inputs = encoder_input, outputs = latent_encoding, name = 'encoder')
+encoder_model = Model(inputs=encoder_input, outputs=latent_encoding,
+                      name='encoder')
 # encoder_model.summary()
 
-decoder_model = Model(inputs = decoder_input, outputs = decoder_output, name = 'decoder')
+decoder_model = Model(inputs=decoder_input, outputs=decoder_output,
+                      name='decoder')
 # decoder_model.summary()
 
 losses = {'decoder_output': 'mse', 'kl_divergence': 'mean_absolute_error'}
-loss_weights = {'decoder_output': 1.0, 'kl_divergence': 1.0}
+loss_weights = {'decoder_output': 1.0, 'kl_divergence': 0.1}
 
-autoencoder.compile(loss = losses, loss_weights = loss_weights, optimizer = 'adam')
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
 
-target_normal = np.zeros((jetList.shape[0], 1))
+autoencoder.compile(loss=losses, loss_weights=loss_weights, optimizer=optimizer)
 
-history = autoencoder.fit(jetList, {'decoder_output': jetList, 'kl_divergence': target_normal}, validation_split = 0.5, batch_size = 50, epochs = 100, verbose = 1)
+target_kl = np.zeros((njets, 1))
+
+history = autoencoder.fit(
+    jetList, {'decoder_output': jetList, 'kl_divergence': target_kl},
+    validation_split=0.5, batch_size=10, epochs=10, verbose=1)

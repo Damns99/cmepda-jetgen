79404036c43c0638cdcd028097139ed4b19599f5
fcattafesta <f.cattafesta@studenti.unipi.it>
2022-02-04 11:49:41 +0100

    Some file opener adjustments

--- utilities/file_opener.py
+++ utilities/file_opener.py
@@ -14,18 +14,19 @@ filename = ['jetImage_7_100p_0_10000.h5', 'jetImage_7_100p_10000_20000.h5',
             'jetImage_7_100p_70000_80000.h5', 'jetImage_7_100p_80000_90000.h5']
 
 
-def fileOpen(test=False):
+def getJetList(test=False):
     '''File opener function for the .h5 dataset.
     '''
-
     jetList = np.array([])
 
     for file in filename:
         f = h5py.File(os.path.join(data_path, file))
         # for pT, etarel, phirel [5, 8, 11]
         myJetList = np.array(f.get("jetConstituentList")[:, :, [5, 8, 11]])
-        jetList = np.concatenate([jetList, myJetList],
-                                 axis=0) if jetList.size else myJetList
+        if jetList.size:
+            jetList = np.concatenate([jetList, myJetList], axis=0)
+        else:
+            jetList = myJetList
         del myJetList
         f.close()
         if test:

87d162af4550372c5a586385255a60cf76d494c1
damns99 <lucarelli.damiano.ld@gmail.com>
2022-02-10 12:12:01 +0100

    figure_saver generator

--- utilities/figure_saver.py
+++ utilities/figure_saver.py
@@ -3,5 +3,5 @@ from matplotlib import pyplot as plt
 
 path = os.path.join(os.path.dirname(__file__), '..', 'trained_models', 'figures')
 
-def saveFig(filename, **kwargs):
-    plt.savefig(os.path.join(path, ''.join([filename, '.pdf'])))
+def saveFig(filename, customName='', **kwargs):
+    plt.savefig(os.path.join(path, ''.join([filename, customName, '.pdf'])))
